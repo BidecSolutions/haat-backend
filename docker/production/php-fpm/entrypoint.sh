e669bac205955e57514e85d34adb4b52a494b9c0
BidecSolutions <nawaz@bidecsol.com>
2026-04-09 21:40:48 +0500

    fix: composer no-scripts in Dockerfile, fix mail server address

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 set -e
 
-# Initialize storage directory if empty
 if [ ! "$(ls -A /var/www/storage)" ]; then
   echo "Initializing storage directory..."
   cp -R /var/www/storage-init/. /var/www/storage
   chown -R www-data:www-data /var/www/storage
 fi
 
-# Remove storage-init directory
 rm -rf /var/www/storage-init
 
+php artisan package:discover --ansi
 php artisan optimize:clear
 
-# Run the default command
 exec "$@"
